2904e83a8f0edd4dba2d97fd1d3a383aaac08558
Victoriaabundrage <victoriaabun@knights.ucf.edu>
2021-03-12 19:18:55 -0500

    Challenge2 commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,12 +10,24 @@ public class PlayerScript : MonoBehaviour
     public float speed;
     public Text score;
     private int scoreValue = 0;
+    public Text winText;
+    public Text loseText;
+    private int lifeValue = 3;
+    public Text Lives;
+
+    public AudioSource musicSource;
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
         score.text = scoreValue.ToString();
+        winText.text = "";
+        Lives.text = "Lives: " + lifeValue.ToString();
+        loseText.text="";
     }
 
     // Update is called once per frame
@@ -25,7 +37,9 @@ public class PlayerScript : MonoBehaviour
         {
         Application.Quit();
         }
+
     }
+    
     void FixedUpdate()
     {
         float hozMovement = Input.GetAxis("Horizontal");
@@ -41,8 +55,37 @@ public class PlayerScript : MonoBehaviour
             scoreValue += 1;
             score.text = scoreValue.ToString();
             Destroy(collision.collider.gameObject);
+
+            if (scoreValue == 4)
+            {
+                transform.position = new Vector3(44.12f , 0.0f, 0.0f);
+                lifeValue = 3;
+                Lives.text = "Lives: " + lifeValue.ToString();
+            }
+
+            if (scoreValue >=8)
+            {
+                musicSource.Stop();
+		        musicSource.clip = musicClipTwo;
+		        musicSource.Play();
+                winText.text = "You win! Game created by Victoria";
+            }
+        }
+
+        if (collision.collider.tag == "Enemy")
+        {
+            lifeValue = lifeValue - 1;
+            Lives.text = "Lives: " + lifeValue.ToString();
+            Destroy(collision.collider.gameObject);
+            if (lifeValue ==0)
+            {
+                loseText.text = "Game Over!";
+                Destroy(this);
+            }
+
         }
     }
+
 
     private void OnCollisionStay2D(Collision2D collision)
     {
